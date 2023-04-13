0f9cf8b4e30dbe39f818e8d9839180b2825dc77f
Giulio <giuliocalzolari@users.noreply.github.com>
2023-04-13 11:34:15 +0200

    "Get that shit outta my master."

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,9 +17,9 @@ wget -q https://get.helm.sh/helm-${helm_version}-linux-amd64.tar.gz \
     && tar xzfv helm-v3*.tar.gz --directory /usr/local/bin/ linux-amd64/helm \
     && rm helm-v3*.tar.gz
 
-echo "using helm@$helm_docs_version"
+echo "using helm-docs@$helm_docs_version"
 wget -q https://github.com/norwoodj/helm-docs/releases/download/v${helm_docs_version}/helm-docs_${helm_docs_version}_Linux_x86_64.tar.gz \
-    && tar xzfv helm-docs_*.tar.gz --directory /usr/local/bin/ helm-docs  \
+    && tar xzf helm-docs_*.tar.gz --directory /usr/local/bin/ helm-docs  \
     && rm helm-docs_*.tar.gz
 
 echo "using yq@$yq_version"
@@ -28,5 +28,5 @@ wget -q https://github.com/mikefarah/yq/releases/download/v$yq_version/yq_linux_
 
 echo "using ct@$ct_version"
 wget -q https://github.com/helm/chart-testing/releases/download/v${ct_version}/chart-testing_${ct_version}_linux_amd64.tar.gz \
-    && tar xzfv chart-testing*.tar.gz --directory /usr/local/bin/ ct \
+    && tar xzf chart-testing*.tar.gz --directory /usr/local/bin/ ct \
     && rm chart-testing*.tar.gz
